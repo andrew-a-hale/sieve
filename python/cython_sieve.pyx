fa061414ac54109dd17c448a0870d5e12bd9e13c
Andrew Hale <andrew@andrewahale.com>
2026-06-12 16:42:16 +1000

    python: opt cython sieve

--- python/cython_sieve.pyx
+++ python/cython_sieve.pyx
@@ -7,7 +7,7 @@ cdef class CSieve:
 
     def __cinit__(self, limit):
         self._bitslength = (limit + 1) // 2
-        self._bits = bytearray(b"\x01") * self._bitslength
+        self._bits = bytearray(b"\xff") * ((self._bitslength + 7) // 8)
 
     def run(self):
         cdef int factor, start, step, i
@@ -19,17 +19,23 @@ cdef class CSieve:
 
         while factor < q:
             for i in range(factor, self._bitslength):
-                if bits[i]:
+                if bits[i >> 3] & (1 << (i & 7)):
                     factor = i
                     break
 
             start = 2 * factor * (factor + 1)
             step = 2 * factor + 1
             while start < self._bitslength:
-                bits[start] = 0
+                bits[start >> 3] &= ~(1 << (start & 7))
                 start += step
 
             factor += 1
 
     def check_primes(self):
-        return self._bits.count(b"\x01")
+        cdef int count = 0
+        cdef int i
+        for i in range(self._bitslength):
+            if self._bits[i >> 3] & (1 << (i & 7)):
+                count += 1
+
+        return count
