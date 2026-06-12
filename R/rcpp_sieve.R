c7a62dce933c5dd2d0db58df8718d7d0272477e7
Andrew Hale <andrew@andrewahale.com>
2026-06-12 16:42:35 +1000

    r: opt rcpp sieve

--- R/rcpp_sieve.R
+++ R/rcpp_sieve.R
@@ -4,8 +4,7 @@ sourceCpp("R/sieve.cpp")
 args <- commandArgs(trailingOnly = TRUE)
 limit <- as.numeric(gsub("_", "", args[1]))
 start <- Sys.time()
-s <- limit |> sieve()
-count <- check_primes(s)
+count <- limit |> sieve()
 duration <- floor(as.numeric(Sys.time() - start) * 1000)
 
 cat(
--- R/sieve.cpp
+++ R/sieve.cpp
@@ -1,10 +1,14 @@
+// [[Rcpp::depends(BH)]]
+
 #include <Rcpp.h>
+#include <boost/dynamic_bitset/dynamic_bitset.hpp>
+
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-std::vector<bool> sieve(size_t limit) {
+long sieve(size_t limit) {
     size_t bitslength = (limit + 1) / 2;
-    std::vector<bool> bits(bitslength, true);
+    boost::dynamic_bitset<> bits(bitslength);
 
     size_t factor = 1;
     double q = sqrt(bitslength / 2) + 1;
@@ -12,34 +16,21 @@ std::vector<bool> sieve(size_t limit) {
 
     while (factor < q) {
         for (size_t i = factor; i < bitslength; i++) {
-            if (bits[i]) {
+            if (!bits[i]) {
                 factor = i;
                 break;
             }
-        } 
+        }
 
         start = 2 * factor * (factor + 1);
         step = 2 * factor + 1;
         while (start < bitslength) {
-            bits[start] = false;
+            bits[start] = true;
             start += step;
         }
 
         factor++;
     }
 
-    return bits;
-}
-
-// [[Rcpp::export]]
-double check_primes(LogicalVector bits) {
-    double count = 0;
-    
-    for (int i = 0; i < bits.size(); i++) {
-        if (bits[i]) {
-            count++;
-        }
-    }
-
-    return count;
+    return bits.size() - bits.count();
 }
